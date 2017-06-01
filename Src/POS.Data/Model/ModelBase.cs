87ef10aeff640cfcba28395ce5bd8dbda15e3351
YuriyKarpenko <yuriy.karpenko@revolutioninsure.com>
2017-06-02 00:08:29 +0300

    prepare models

---
+++ Src/POS.Data/Model/ModelBase.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace POS.Data.Model
+{
+	public abstract class BaseModel<TKey> : INotifyPropertyChanged
+	{
+		private event PropertyChangedEventHandler _propertyChanged;
+		event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged { add { _propertyChanged += value; } remove { _propertyChanged -= value; } }
+
+		//private IT.MemCache<string, PropertyInfo> _cache = new IT.MemCache<string, PropertyInfo>();
+
+		[DataMember]
+		public TKey Id { get; set; }
+
+		public abstract bool IsPersisted { get; }
+
+
+		protected virtual void OnPropertyChanged(String propertyName)
+		{
+			if (_propertyChanged != null)
+			{
+				_propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+		protected T Get<T>(string name)
+		{
+			var p = pi(name);
+			return (T)p.GetValue(this);
+		}
+
+		protected void Set(string name, object value)
+		{
+			var p = pi(name);
+			var v = p.GetValue(this);
+			if (!value.Equals(v))
+			{
+				p.SetValue(this, v);
+				OnPropertyChanged(name);
+			}
+		}
+		protected void SetNav(string name, object value)
+		{
+			var p = pi(name);
+			var v = p.GetValue(this);
+			if (!ReferenceEquals(value, v))
+			{
+				p.SetValue(this, v);
+				OnPropertyChanged(name);
+			}
+		}
+
+
+		private FieldInfo pi(string name)
+		{
+			//return _cache[name, () => this.GetType().GetProperty(name)];
+			return this.GetType().GetField("_"+name);
+		}
+	}
+
+	public abstract class BaseModel : BaseModel<Guid>
+	{
+		public override bool IsPersisted => Id != default(Guid);
+	}
+
+	public abstract class PersistedModel : BaseModel
+	{
+		[DataMember]
+		public DateTime DateCreated
+		{
+			get { return _DateCreated; }
+			set { Set(nameof(DateCreated), value); }
+		}
+		private DateTime _DateCreated;
+
+		[DataMember]
+		public DateTime DateLastModified
+		{
+			get { return _DateLastModified; }
+			set { Set(nameof(DateLastModified), value); }
+		}
+		private DateTime _DateLastModified;
+	}
+
+	public abstract class DictionaryModel : PersistedModel
+	{
+		[DataMember]
+		public string Name
+		{
+			get { return _Name; }
+			set { Set(nameof(Name), value); }
+		}
+		private string _Name;
+
+		[DataMember]
+		public bool Hidden
+		{
+			get { return _Hidden; }
+			set { Set(nameof(Hidden), value); }
+		}
+		private bool _Hidden;
+
+	}
+
+	[KnownType(typeof(User))]
+	public abstract class PersistedUserModel : PersistedModel
+	{
+		[DataMember]
+		public Guid CreatedByUserId
+		{
+			get { return _CreatedByUserId; }
+			set { Set(nameof(CreatedByUserId), value); }
+		}
+		private Guid _CreatedByUserId;
+
+
+		[DataMember]
+		public User CreatedByUser
+		{
+			get { return _CreatedByUser; }
+			set { SetNav(nameof(CreatedByUser), value); }
+		}
+		private User _CreatedByUser;
+	}
+
+	[KnownType(typeof(User))]
+	public abstract class PersistedUser2Model : PersistedUserModel
+	{
+		[DataMember]
+		public Guid? ModifiedByUserId
+		{
+			get { return _ModifiedByUserId; }
+			set { Set(nameof(ModifiedByUserId), value); }
+		}
+		private Guid? _ModifiedByUserId;
+
+
+		[DataMember]
+		public User ModifiedByUser
+		{
+			get { return _ModifiedByUser; }
+			set { SetNav(nameof(ModifiedByUser), value); }
+		}
+		private User _ModifiedByUser;
+	}
+
+}
--- Src/POS.Data/Model/Price.cs
+++ Src/POS.Data/Model/Price.cs
@@ -1,131 +1,59 @@
 using System;
-using System.ComponentModel;
 using System.Runtime.Serialization;
 
 namespace POS.Data.Model
 {
-    [DataContract(IsReference = true)]
-    [KnownType(typeof(MenuItem))]
-    [KnownType(typeof(PriceList))]
-    public partial class Price: INotifyPropertyChanged
-    {
-        #region Primitive Properties
-    
-        [DataMember]
-        public System.Guid Id
-        {
-            get { return _id; }
-            set
-            {
-                if (_id != value)
-                {
-                    _id = value;
-                    OnPropertyChanged("Id");
-                }
-            }
-        }
-        private System.Guid _id;
-    
-        [DataMember]
-        public System.Guid PriceListId
-        {
-            get { return _priceListId; }
-            set
-            {
-                if (_priceListId != value)
-                {
-                    _priceListId = value;
-                    OnPropertyChanged("PriceListId");
-                }
-            }
-        }
-        private System.Guid _priceListId;
-    
-        [DataMember]
-        public System.Guid MenuItemId
-        {
-            get { return _menuItemId; }
-            set
-            {
-                if (_menuItemId != value)
-                {
-                    _menuItemId = value;
-                    OnPropertyChanged("MenuItemId");
-                }
-            }
-        }
-        private System.Guid _menuItemId;
-    
-        [DataMember]
-        public decimal Price1
-        {
-            get { return _price1; }
-            set
-            {
-                if (_price1 != value)
-                {
-                    _price1 = value;
-                    OnPropertyChanged("Price1");
-                }
-            }
-        }
-        private decimal _price1;
+	[DataContract(IsReference = true)]
+	[KnownType(typeof(MenuItem))]
+	[KnownType(typeof(PriceList))]
+	public partial class Price : BaseModel
+	{
+		#region Primitive Properties
 
-        #endregion
-        #region Navigation Properties
-    
-        [DataMember]
-        public MenuItem MenuItem
-        {
-            get { return _menuItem; }
-            set
-            {
-                if (!ReferenceEquals(_menuItem, value))
-                {
-                    _menuItem = value;
-                    OnNavigationPropertyChanged("MenuItem");
-                }
-            }
-        }
-        private MenuItem _menuItem;
-    
-        [DataMember]
-        public PriceList PriceList
-        {
-            get { return _priceList; }
-            set
-            {
-                if (!ReferenceEquals(_priceList, value))
-                {
-                    _priceList = value;
-                    OnNavigationPropertyChanged("PriceList");
-                }
-            }
-        }
-        private PriceList _priceList;
+		[DataMember]
+		public Guid PriceListId
+		{
+			get { return _PriceListId; }
+			set { Set(nameof(PriceListId), value); }
+		}
+		private Guid _PriceListId;
 
-        #endregion
-        #region ChangeTracking
-    
-        protected virtual void OnPropertyChanged(String propertyName)
-        {
-            if (_propertyChanged != null)
-            {
-                _propertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-    
-        protected virtual void OnNavigationPropertyChanged(String propertyName)
-        {
-            if (_propertyChanged != null)
-            {
-                _propertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-    
-        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged{ add { _propertyChanged += value; } remove { _propertyChanged -= value; } }
-        private event PropertyChangedEventHandler _propertyChanged;
+		[DataMember]
+		public Guid MenuItemId
+		{
+			get { return _MenuItemId; }
+			set { Set(nameof(MenuItemId), value); }
+		}
+		private Guid _MenuItemId;
 
-        #endregion
-    }
+		[DataMember]
+		public decimal Amount
+		{
+			get { return _Amount; }
+			set { Set(nameof(Amount), value); }
+		}
+		private decimal _Amount;
+
+		#endregion
+
+		#region Navigation Properties
+
+		[DataMember]
+		public MenuItem MenuItem
+		{
+			get { return _MenuItem; }
+			set { SetNav(nameof(MenuItem), value); }
+		}
+		private MenuItem _MenuItem;
+
+		[DataMember]
+		public PriceList PriceList
+		{
+			get { return _PriceList; }
+			set { Set(nameof(PriceList), value); }
+		}
+		private PriceList _PriceList;
+
+		#endregion
+	}
 }
